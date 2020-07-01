e695e8d23a6a90e014777854beba5f06bbfb32b3
Atajan <63938575+gatajan@users.noreply.github.com>
2020-07-01 01:48:51 -0500

    Update Person.java

--- src/main/java/JavaCode/Person.java
+++ src/main/java/JavaCode/Person.java
@@ -3,6 +3,7 @@ package JavaCode;
 public class Person {
     int age;
 private String name;
+    private int ID;
 
     public static void main(String[] args) {
         System.out.println("Hello World");
